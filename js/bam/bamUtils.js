d3bcf3bd4e7bce6d1b51fe80a7a4e6fc65651782
David Ma <david.ma@petermac.org>
2019-03-25 10:49:02 +1100

    Version stamp

--- js/bam/bamUtils.js
+++ js/bam/bamUtils.js
@@ -45,7 +45,6 @@ var igv = (function (igv) {
     const DEFAULT_SAMPLING_WINDOW_SIZE = 100;
     const MAXIMUM_SAMPLING_DEPTH = 100000; // DKGM - Raising the maximum here because pmac people prefer to view more reads at the of risk crashing their browsers. I have provided appropriate dialog boxes to warn them of this when changing this setting.
     const DEFAULT_SAMPLING_DEPTH = 100;
-    const MAXIMUM_SAMPLING_DEPTH = 2500;
 
     igv.BamUtils = {
 
--- js/igv-create.js
+++ js/igv-create.js
@@ -27,7 +27,7 @@ var igv = (function (igv) {
 
 
     var igvjs_version = "@VERSION";
-    igv.version = "Peter Mac build: 21-Aug-2018, igv.js v2.0.0 rc 4"; // DKGM - Update this before building so we can easily query the version in production
+    igv.version = "Peter Mac build: 25-Mar-2019, igv.js v2.2.5"; // DKGM - Update this before building so we can easily query the version in production
 
     let allBrowsers = [];
 
